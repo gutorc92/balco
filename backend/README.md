b6877f8864c5aa67a60c58cc4e6191c197478efc
Gustavo Coelho <gutorc@hotmail.com>
2020-01-09 21:00:18 -0300

    Tests
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

---
+++ backend/README.md
@@ -0,0 +1,29 @@
+# API de cálculos
+
+# Instalando e configurando
+
+A API foi feita visando o uso de Python3.
+
+>$ pip install -r requirements.txt
+
+*Obs.*
+
+Recomenda-se fortemente que se tenha um ambiente virtual dedicado por aplicações, para evitar comprometer a estabildiade do seu sistema operacional. Utilize o [VirtualEnvWrapper](https://virtualenvwrapper.readthedocs.io/en/latest/) ou um container [Docker](https://hub.docker.com/_/python/).
+
+
+# Executando
+
+> $ python api.py
+
+A API deve estar disponivel na porta 5000.
+
+
+# Executando os testes
+
+Entre na pasta tests
+
+> $ python teste_api.py
+
+# Executando o linter
+
+> $ pylint *.py
---
+++ backend/tests/test_api.py
@@ -0,0 +1,31 @@
+import unittest
+import json
+import urllib.request
+from flask_testing import TestCase
+import sys
+sys.path.append('..')
+from api import app
+
+class MyTest(TestCase):
+
+	def create_app(self):
+		app.config['TESTING'] = True
+		# app.config['LIVESERVER_PORT'] = 8943
+		# # Default timeout is 5 seconds
+		# app.config['LIVESERVER_TIMEOUT'] = 10
+		return app
+	
+	def test_post(self):
+		data = {
+			'array': [3, 4, 1, 7, 8, 5],
+			'K': 2,
+			'L': 3
+		}
+		with self.app.test_client() as c:
+			response = c.post('/calculate', json=data)
+			self.assertEqual(response.status_code, 200)
+			result = response.get_json()
+			self.assertEqual(result['total'], 27)
+
+if __name__ == '__main__':
+	unittest.main()
